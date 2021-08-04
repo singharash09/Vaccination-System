7042e1f3764ac528afca93d136be3121cac4dedc
Arash Manpreet Singh <singharash09@gmail.com>
2021-08-04 19:41:45 -0400

    minor

---
+++ public/vaccination.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'templates/header.php';
+include_once '../config/db.php';
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        
+    </head>
+
+    <body>
+
+    </body>
+</html>
