25981427cb086b11784655b55ca86cf3c52f93cd
Arash Manpreet Singh <singharash09@gmail.com>
2021-08-07 10:25:28 -0400

    minor formatting

--- public/vaccination/vaccination.php
+++ public/vaccination/vaccination.php
@@ -17,38 +17,26 @@ include_once '../templates/header.php';
             <div class="row top-buffer">
                 <div class="col-sm-9"><h1 class="page-title"> <i class="fas fa-syringe"></i>  Vaccination</h1></div>
             </div>
-        </div>       
-
-        <div>
-
-      <div class="row">
-
-        <div class="col-lg-12 center-element">
-            <a href="#" class="btn btn-sq-lg btn-primary" style="display: inline-block; text-align: center; margin:20px; padding: 5vw; font-weight: bolder;">
-            <i class="fas fa-flask fa-5x"></i></br></br>
-            Vaccine Types
-            </a>      
-            <a href="#" class="btn btn-sq-lg btn-primary" style="display: inline-block; text-align: center; margin:20px; padding: 5vw; font-weight: bolder;">
-            <i class="fas fa-table  fa-5x"></i></br></br>
-            Vaccinations
-            </a>    
-            <a href="#" class="btn btn-sq-lg btn-primary" style="display: inline-block;text-align: center; margin:20px;  padding: 5vw; font-weight: bolder;">
-            <i class="fas fa-user-circle fa-5x"></i></br></br>
-             &nbsp;&nbsp;&nbsp;Age Group &nbsp;&nbsp;&nbsp;
-            </a>  
-            <a href="#" class="btn btn-sq-lg btn-primary" style="display: inline-block; text-align: center; margin:20px; padding: 5vw; font-weight: bolder;">
-            <i class="fas fa-map fa-5x"></i></br></br>
-             &nbsp;&nbsp;&nbsp;&nbsp;Province&nbsp;&nbsp;&nbsp;&nbsp;
-            </a>         
+            
+            <div class="row">
+                <div class="col-lg-12 center-element">
+                    <a href="#" class="btn btn-sq-lg btn-primary" style="display: inline-block; text-align: center; margin:20px; padding: 5vw; font-weight: bolder;">
+                    <i class="fas fa-flask fa-5x"></i></br></br>
+                    Vaccine Types
+                    </a>      
+                    <a href="#" class="btn btn-sq-lg btn-primary" style="display: inline-block; text-align: center; margin:20px; padding: 5vw; font-weight: bolder;">
+                    <i class="fas fa-table  fa-5x"></i></br></br>
+                    Vaccinations
+                    </a>    
+                    <a href="#" class="btn btn-sq-lg btn-primary" style="display: inline-block;text-align: center; margin:20px;  padding: 5vw; font-weight: bolder;">
+                    <i class="fas fa-user-circle fa-5x"></i></br></br>
+                    &nbsp;&nbsp;&nbsp;Age Group &nbsp;&nbsp;&nbsp;
+                    </a>  
+                    <a href="#" class="btn btn-sq-lg btn-primary" style="display: inline-block; text-align: center; margin:20px; padding: 5vw; font-weight: bolder;">
+                    <i class="fas fa-map fa-5x"></i></br></br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;Province&nbsp;&nbsp;&nbsp;&nbsp;
+                </a>            
+            </div>
         </div>
-      </div>
-
-
-        </div>
-
-
-
-
-
     </body>
 </html>
