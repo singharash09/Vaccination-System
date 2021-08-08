b7993fba58e0339861a48ff2da4917e14adbd0d8
Sinnsu1 <suthan1998sinnathurai@hotmail.com>
2021-08-08 13:28:04 -0400

    Added the if statement in case the End_date is null

--- lib/processEmployeeInsertion.php
+++ lib/processEmployeeInsertion.php
@@ -17,8 +17,25 @@ $employeeEndDate = $_POST['employeeEndDate'];
 
 
 
-
+if(empty($_POST['employeeEndDate'] )){
   
+    $query1 = "INSERT INTO HealthCare_Worker VALUES('$employeeSSN', '$employeeEID');";
+    $query2 = "INSERT INTO Works_At VALUES('$employeeSSN', '$employeeFacility','$employeeStartDate',null);";
+
+    
+  
+    $successQuery1 = mysqli_query($conn, $query1);
+    $successQuery2 = mysqli_query($conn, $query2);
+
+    
+    if(!$successQuery1 ||  !$successQuery2){
+        header("Location: ../public/employees/insertEmployee.php?insertion=failed");
+        
+    }else{
+        header("Location: ../public/employees/employees.php?insertion=success");
+    }
+
+} else {
     $query1 = "INSERT INTO HealthCare_Worker VALUES('$employeeSSN', '$employeeEID');";
     $query2 = "INSERT INTO Works_At VALUES('$employeeSSN', '$employeeFacility','$employeeStartDate','$employeeEndDate');";
 
@@ -34,8 +51,7 @@ $employeeEndDate = $_POST['employeeEndDate'];
     }else{
         header("Location: ../public/employees/employees.php?insertion=success");
     }
-
-  
+}
 
 
 ?>
