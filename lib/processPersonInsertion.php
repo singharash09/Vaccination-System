da42e282008721cd2b275a9be5702d16431bb6ad
Arash Manpreet Singh <singharash09@gmail.com>
2021-08-08 13:24:06 -0400

    insert person with employee

--- lib/processPersonInsertion.php
+++ lib/processPersonInsertion.php
@@ -15,6 +15,8 @@ $personCity = $_POST['personCity'];
 $personProvince = $_POST['personProvince'];
 $personPostalCode= $_POST['personPostalCode'];
 $employeeInput = $_POST['employeeInput'];
+$employeeFacility = $_POST['employeeFacility'];
+$employeeStartDate = $_POST['employeeStartDate'];
 
 // echo "SSN: " . $personSSN . "\n";
 // echo "medicare: ". $personMedicare . "\n";
@@ -27,9 +29,11 @@ $employeeInput = $_POST['employeeInput'];
 // echo "CITIZENSHIP: " . $personCitizenship . "\n";
 // echo "ADDRESS: " . $personAddress . "\n";
 // echo "POSTAL CODE: ". $personPostalCode . "\n";
+// echo "employee eid: ".$employeeEID."\n";
+// echo "employee facility: ".$employeeFacility."\n";
+// echo "employee start date: ".$employeeStartDate."\n";
 
-
-if(empty($_POST['employeeInput'])){
+if(empty($_POST['employeeInput']) ||  empty($_POST['employeeFacility']) || empty($_POST['employeeStartDate'])){
 
     echo "inside the empty emloyee";
     $query1 = "INSERT INTO Postal_Code  (postal_code, city, province_code) VALUES('$personPostalCode', '$personCity', '$personProvince') 
@@ -56,15 +60,16 @@ if(empty($_POST['employeeInput'])){
  
     $query3 = "INSERT INTO HealthCare_Worker VALUES ('$personSSN', '$employeeInput');";
 
+    $query4 = "INSERT INTO Works_At VALUES('$personSSN', '$employeeFacility','$employeeStartDate', NULL);";
 
     echo "inside the non empty employee";
 
     mysqli_query($conn, $query1);
     $successQuery2 = mysqli_query($conn, $query2); 
     $successQuery3 = mysqli_query($conn, $query3);
-    if(!$successQuery2 ||  !$successQuery3){
+    $successQuery4 = mysqli_query($conn, $query4);
+    if(!$successQuery2 ||  !$successQuery3 ||  !$successQuery4){
         header("Location: ../public/people/insertPerson.php?insertion=failed");
-        
     }else{
         header("Location: ../public/people/people.php?insertion=success");
     }
--- public/people/insertPerson.php
+++ public/people/insertPerson.php
@@ -108,14 +108,36 @@ if(isset($_GET['insertion'])){
                                   ?>
                                 </select> 
                           </div>
-                          <div class="col-sm-2">
+                          <div class="col-sm-12">
                                 <input type="checkbox" id="isEmployee" name="isEmployee" value="isEmployee">
                                 <label for="isEmployee"> Is Employee?</label><br>
                           </div>                         
-                           <div class="col-sm-12">
+                           <div class="col-sm-6">
                               <label id="employeeLabel"for="employeeInput" class="form-label">Employee ID</label>
                               <input id="employeeInput" type="text" class="form-control" id="employeeInput"  name="employeeInput" placeholder="123456789" minlength="9" maxlength="9"> 
                           </div>
+                          <div class="col-sm-3">
+                            <label id="employeeFacilityLabel" for="employeeFacility" class="form-label">Facility Name</label>
+                              <select class="form-select" name="employeeFacility" id="employeeFacility" aria-label="Select Type">
+                                <option>Select</option>
+                                  <?php
+                                  $query1 = "SELECT facility_name FROM Vaccination_Facility;";
+                                  $result = mysqli_query($conn, $query1);
+
+                                  $resultCheck = mysqli_num_rows($result);
+                                  if($resultCheck>0){
+                                      while($row = mysqli_fetch_assoc($result)){
+                                          echo '<option value="'.$row['facility_name'].'">'.$row['facility_name'].'</option>';
+                                        }
+                                    }
+                                  ?>
+                                </select> 
+                          </div>
+                          <div class="col-sm-3">
+                              <label id="employeeStartDateLabel"  for="employeeStartDate" class="form-label">Start Date</label>
+                              <input type="date" class="form-control" id="employeeStartDate" name="employeeStartDate">                              
+                          </div>
+
                           <div class="col-sm-12">
                            <button class="btn btn-success" type="submit">Add person</button>
                           </div>                                                     
@@ -134,6 +156,12 @@ if(isset($_GET['insertion'])){
             document.getElementById("employeeLabel").style.display= 'none';
             document.getElementById("employeeInput").style.display= 'none';
 
+            document.getElementById("employeeFacilityLabel").style.display= 'none';           
+            document.getElementById("employeeFacility").style.display= 'none';
+
+            document.getElementById("employeeStartDateLabel").style.display= 'none';            
+            document.getElementById("employeeStartDate").style.display= 'none';         
+
             let element = document.getElementById("isEmployee");
             document.getElementById("isEmployee").onchange = function(){
                 let isChecked = this.checked;
@@ -141,9 +169,28 @@ if(isset($_GET['insertion'])){
                 if(isChecked){
                     document.getElementById("employeeLabel").style.display= 'block';
                     document.getElementById("employeeInput").style.display= 'block';
+                    document.getElementById("employeeInput").required = true;
+
+                    document.getElementById("employeeFacilityLabel").style.display= 'block';                     
+                    document.getElementById("employeeFacility").style.display= 'block';
+                    document.getElementById("employeeFacility").required = true;   
+
+                    document.getElementById("employeeStartDateLabel").style.display= 'block';                  
+                    document.getElementById("employeeStartDate").style.display= 'block'; 
+                    document.getElementById("employeeStartDate").required = true;  
+
                 }else{
                     document.getElementById("employeeLabel").style.display= 'none';
                     document.getElementById("employeeInput").style.display= 'none';
+                    document.getElementById("employeeStartDate").required = false;
+
+                    document.getElementById("employeeFacilityLabel").style.display= 'none';           
+                    document.getElementById("employeeFacility").style.display= 'none';
+                    document.getElementById("employeeStartDate").required = false;
+
+                    document.getElementById("employeeStartDateLabel").style.display= 'none';            
+                    document.getElementById("employeeStartDate").style.display= 'none';
+                    document.getElementById("employeeStartDate").required = false;                 
                 }
             }
 
