c3ccfe812349edae599d2bbf462158ff827649eb
imullett <ianmullett@gmail.com>
2021-08-09 01:17:28 -0230

    Update processVariantEdit.php

--- lib/processVariantEdit.php
+++ lib/processVariantEdit.php
@@ -4,11 +4,7 @@ include_once '../config/db.php';
 $newType = $_POST['type_of_infection'];
 $variantToEdit =$_GET['variantToEdit'];
 
-
 echo "Variant: " . $newType . "\n";
-
-$query1 = "INSERT INTO Postal_Code  (postal_code, city, province_code) VALUES('$personPostalCode', '$personCity', '$personProvince') 
-ON DUPLICATE KEY UPDATE city='$personCity', province_code='$personProvince';";
 
 $query = "UPDATE Infection_Type 
 SET type_of_infection='$newType'
